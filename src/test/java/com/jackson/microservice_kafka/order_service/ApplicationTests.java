f016e2549d4e41a7fa8c262988b5db85bc32a4aa
jackson1404 <jackson14042004@gmail.com>
2025-07-06 16:35:22 +0630

    added frame for order service

---
+++ src/test/java/com/jackson/microservice_kafka/order_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.microservice_kafka.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
