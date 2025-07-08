9303de7fea5a7c61d679dafeb989d8d2919a4c18
Win Min Thant <jackson14042004@gmail.com>
2025-07-08 15:12:10 +0630

    added for schedular

---
+++ src/main/java/com/jackson/microservice_kafka/order_service/schedular/InventoryCheckSchedular.java
@@ -0,0 +1,25 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.jackson.microservice_kafka.order_service.schedular;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * InevntoryCheckSchedular Class.
+ * <p>
+ * </p>
+ *
+ * @author
+ */
+@Service
+@RequiredArgsConstructor
+@Data
+public class InventoryCheckSchedular {
+}
